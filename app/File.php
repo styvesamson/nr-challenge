00b0f4983a09b8ecada6a8876590a6eeca5448b6
Yves Toupe <root@nr-challenge>
2016-04-22 04:39:40 +0000

    first commit

---
+++ app/File.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+
+class File extends Eloquent {
+
+    protected $connection = 'mongodb';
+    protected $collection = 'file';
+
+}
---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\File ;
+
+class FileController extends Controller
+{
+    //
+    public function index(){
+        $file = new File();
+        $file->name = uniqid();
+        $file->save();
+        var_dump(File::all());
+        exit;
+        
+      
+        
+    }
+}
